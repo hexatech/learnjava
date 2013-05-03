0a90864e29d50241aedeadba0521611cfdc8d498
bhupen <hubbyloveswiffy@gmail.com>
2013-05-03 23:28:42 +0800

    Changed the brokerage for MF global Broker

--- src/main/java/com/learn/observerpattern/MFBroker.java
+++ src/main/java/com/learn/observerpattern/MFBroker.java
@@ -4,7 +4,7 @@ public class MFBroker implements IBroker {
 
 	public void calculateCommision(double exchangeRate) {
 		System.out.println("MF broker charged commision of "
-				+ (exchangeRate * 0.02));
+				+ (exchangeRate * 0.04));
 	}
 
 }
